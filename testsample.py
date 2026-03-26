220a0b1100d18e4ef0466eb4f179183c1c56ec70
gagan770013-lab <gagan770013@gmail.com>
2026-03-26 21:57:02 +0530

    Create testsample.py

---
+++ testsample.py
@@ -0,0 +1,15 @@
+def linear_search(list, n, key):
+    for i in range(0, n):
+        if (list[i] == key):
+            return i
+    return -1
+
+list = [9, 12, 15, 37, 28, 30]
+n = len(list)
+key = 9
+
+result = linear_search(list, n, key)
+if (result == -1):
+    print("Element not found")
+else:
+    print("Element found at the position", result + 1)
